eebd900c9cadfae0e05596feba95fa5795bf9248
Joshua Carroll <carrolljosh1976@gmail.com>
2025-09-07 11:31:35 -0400

    added middleware file for CLerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       <h1>Hello Stream!</h1>
+      <Button>Click Me!</Button>
     </div>
   );
 }
