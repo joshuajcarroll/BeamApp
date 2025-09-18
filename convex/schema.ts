1e38368ff170a6dc516f6f66d90c72b0dfac1c33
Joshua Carroll <carrolljosh1976@gmail.com>
2025-09-18 10:48:57 -0400

    fixed schema for users in database

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,12 +1,12 @@
 import { defineTable, defineSchema } from "convex/server";
 import { v } from "convex/values";
 
-export const schema = defineSchema({
+export default defineSchema({
   users: defineTable({
     userId: v.string(),
     name: v.string(),
     email: v.string(),
-    createdAt: v.string(),
+    imageUrl: v.string(),
   })
     .index("by_userId", ["userId"])
     .index("by_email", ["email"]),
