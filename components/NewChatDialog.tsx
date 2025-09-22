178bc718047e49ee7b12dfb1c97ba5f6b3f31ce1
Joshua Carroll <carrolljosh1976@gmail.com>
2025-09-22 13:15:42 -0400

    added helper functions for adding and removing users to a chat in NewChatDialog component

--- components/NewChatDialog.tsx
+++ components/NewChatDialog.tsx
@@ -14,5 +14,16 @@ export function NewChatDialog({ children }: { children: React.ReactNode }) {
   const { user } = useUser();
   const setActiveChannel = useChatContext;
 
+  const handleSelectUser = (user: Doc<"users">) => {
+    //Avoid adding the same user twice
+    if (!selectedUsers.find((u) => u._id === user._id)) {
+      setSelectedUsers((prev) => [...prev, user]);
+    }
+  };
+
+  const removeUser = (userId: string) => {
+    setSelectedUsers((prev) => prev.filter((user) => user._id !== userId));
+  };
+
   return <>{children}</>;
 }
